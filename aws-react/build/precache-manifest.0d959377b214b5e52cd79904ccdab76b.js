1242ebde2fd029ce06b9e313f29c7904062554a5
Aditya V B <adiballurkar34@gmail.com>
2019-11-03 16:03:08 +0530

    build

---
+++ aws-react/build/precache-manifest.0d959377b214b5e52cd79904ccdab76b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24093ccc9350101e747e1e63c1aecce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "286c6dcd838bc1ff7a46",
+    "url": "/static/css/main.9f7c9245.chunk.css"
+  },
+  {
+    "revision": "28fd3e00e3081899188b",
+    "url": "/static/js/2.9f956a9a.chunk.js"
+  },
+  {
+    "revision": "286c6dcd838bc1ff7a46",
+    "url": "/static/js/main.c18baf5f.chunk.js"
+  },
+  {
+    "revision": "d979123841318035fbf7",
+    "url": "/static/js/runtime-main.1c03b009.js"
+  }
+]);

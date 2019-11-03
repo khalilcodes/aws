dbb3e38915c8d4e3aa3ff4c489171cff192034e5
Aditya V B <adiballurkar34@gmail.com>
2019-11-03 14:10:29 +0530

    app.js adi

--- aws-react/src/App.js
+++ aws-react/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
 import Profile from './components/profile';
+import Mainpage from './components/MainPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div>
       <Navbar />
       <SearchBar />
       <Profile />
+      <Switch>
+        <Route path="/mainpage">
+          <Mainpage />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
